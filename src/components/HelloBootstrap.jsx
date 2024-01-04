99745ef32cdf9dc5220568237a036f27caf89edc
Hamda <nurfore14@gmail.com>
2024-01-04 01:33:22 +0000

    fixed err

--- src/components/HelloBootstrap.jsx
+++ src/components/HelloBootstrap.jsx
@@ -3,49 +3,49 @@ import React from "react";
 function HelloBootstrap() {
     return (
         // bootstrap navbar, jumbotron and cards
-        <div classNameName="container">
-            <nav classNameName="navbar navbar-expand-lg bg-body-tertiary">
-                <div classNameName="container-fluid">
-                    <a classNameName="navbar-brand" href="#">Navbar</a>
-                    <button classNameName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span classNameName="navbar-toggler-icon"></span>
+        <div className="container">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li classNameName="nav-item">
-                                <a classNameName="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li classNameName="nav-item">
-                                <a classNameName="nav-link" href="#">Link</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
                             </li>
-                            <li classNameName="nav-item dropdown">
-                                <a classNameName="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
                                 </a>
-                                <ul classNameName="dropdown-menu">
-                                    <li><a classNameName="dropdown-item" href="#">Action</a></li>
-                                    <li><a classNameName="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr classNameName="dropdown-divider"></hr></li>
-                                    <li><a classNameName="dropdown-item" href="#">Something else here</a></li>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"></hr></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
                             </li>
-                            <li classNameName="nav-item">
-                                <a classNameName="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
                             </li>
                         </ul>
-                        <form classNameName="d-flex" role="search">
-                            <input classNameName="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button classNameName="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button className="btn btn-outline-success" type="submit">Search</button>
                             </input>
                         </form>
                     </div>
                 </div>
             </nav>
-            <div classNameName="p-5 mb-4 bg-body-tertiary rounded-3">
-                <div classNameName="container-fluid py-5">
-                    <h1 classNameName="display-5 fw-bold">Custom jumbotron</h1>
-                    <p classNameName="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                    <button classNameName="btn btn-primary btn-lg" type="button">Example button</button>
+            <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+                    <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                    <button className="btn btn-primary btn-lg" type="button">Example button</button>
                 </div>
             </div>
             <div className="card" style={{width: '18rem'}}>
