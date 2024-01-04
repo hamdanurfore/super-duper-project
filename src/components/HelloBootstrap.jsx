6aa4cba3e6ddc3d42548c9711c0d093303e84413
Hamda <nurfore14@gmail.com>
2024-01-04 01:49:41 +0000

    fixed all other err

--- src/components/HelloBootstrap.jsx
+++ src/components/HelloBootstrap.jsx
@@ -34,9 +34,12 @@ function HelloBootstrap() {
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                            </input>
+                            <input className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
@@ -48,14 +51,12 @@ function HelloBootstrap() {
                     <button className="btn btn-primary btn-lg" type="button">Example button</button>
                 </div>
             </div>
-            <div className="card" style={{width: '18rem'}}>
-                <img src="..." className="card-img-top" alt="...">
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                    </img>
+            <div className="card" style={{ width: '18rem' }}>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
         </div>
     )
